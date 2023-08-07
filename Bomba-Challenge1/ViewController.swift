09b8c5d239d3ad0e9c60c83e2ab6fb0d7457010e
DmitryLorents <zmey.task@gmail.com>
2023-08-07 11:26:10 +0300

    Initial Commit

---
+++ Bomba-Challenge1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bomba-Challenge1
+//
+//  Created by Дмитрий Лоренц on 07.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
