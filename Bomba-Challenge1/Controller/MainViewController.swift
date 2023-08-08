002fc6ee917fa914702aa45b3e99b973fc5286c8
DmitryLorents <zmey.task@gmail.com>
2023-08-08 14:06:12 +0300

    Add 3 buttons to main screen

--- Bomba-Challenge1/Controller/MainViewController.swift
+++ Bomba-Challenge1/Controller/MainViewController.swift
@@ -6,24 +6,85 @@
 //
 
 import UIKit
+import SnapKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
+    
+    //MARK: Propperties
+    var backgroungImageView: UIImageView = {
+        let backgroundImage = UIImage(named: "backgroundImage")
+        let imageView = UIImageView(image: backgroundImage)
+        return imageView
+    }()
+    var rulesButton: UIButton = {
+        let button = UIButton()
+        let rulesImage = UIImage(named: "rules")
+        button.setImage(rulesImage, for: .normal)
+        return button
+    }()
+    
+    var startGameButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .purpleButton
+        button.setTitle("Старт игры", for: .normal)
+        button.tintColor = .yellowText
+        
+        return button
+    }()
+    
+    var categoryButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .purpleButton
+        button.setTitle("Категории", for: .normal)
+        button.tintColor = .yellowText
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setOutlets()
+        setConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        roundCornersForButtons()
     }
-    */
+    
+//MARK: Methods
+    func setOutlets() {
+        view.addSubview(backgroungImageView)
+        view.addSubview(rulesButton)
+        view.addSubview(categoryButton)
+        view.addSubview(startGameButton)
+        
+    }
+    
+    func setConstraints() {
+        backgroungImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        rulesButton.snp.makeConstraints { make in
+            make.height.width.equalTo(58)
+            make.bottom.trailing.equalToSuperview().inset(17)
+        }
+        
+        categoryButton.snp.makeConstraints { make in
+            make.height.equalTo(79)
+            make.leading.trailing.equalToSuperview().inset(50)
+            make.bottom.equalToSuperview().inset(94)
+        }
+        
+        startGameButton.snp.makeConstraints { make in
+            make.leading.trailing.height.equalTo(categoryButton)
+            make.bottom.equalTo(categoryButton.snp.top).inset(-15)
+        }
+        
+    }
+    
+    fileprivate func roundCornersForButtons() {
+        startGameButton.layer.cornerRadius = startGameButton.bounds.height / 2
+        categoryButton.layer.cornerRadius = categoryButton.bounds.height / 2
+    }
 
 }
