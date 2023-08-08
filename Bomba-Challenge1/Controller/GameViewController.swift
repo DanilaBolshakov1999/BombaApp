79bf87164f0ca959678bbe353972fce6459f8dae
Danila Bolshakov <danila1999777@gmail.com>
2023-08-08 10:09:30 +0300

    creating a gameViewController UI for further improvements
    I created a screen in it there are two stacks one under the navigationController the other under the label, image, button and these two stacks are located in the mainStack

--- Bomba-Challenge1/Controller/GameViewController.swift
+++ Bomba-Challenge1/Controller/GameViewController.swift
@@ -5,4 +5,119 @@
 //  Created by iOS - Developer on 07.08.2023.
 //
 
-import Foundation
+import UIKit
+
+final class GameViewController: UIViewController {
+    
+    //MARK: - UI
+    private lazy var backgroundView: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: "backgroundImage")
+        element.contentMode = .scaleAspectFill
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var mainTwoStackView: UIStackView = {
+        let element = UIStackView()
+        element.axis = .vertical
+        element.spacing = 5
+        element.alignment = .center
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var mainOneStackView: UIStackView = {
+        let element = UIStackView()
+        element.axis = .horizontal
+        element.spacing = 3
+        element.alignment = .center
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var mainLabelView: UILabel = {
+        let label = UILabel()
+        label.text = """
+        Нажмите
+        "Запустить" чтобы
+        начать игру
+        """
+        label.numberOfLines = 0
+        label.textColor = UIColor(red: 139/255, green: 60/255, blue: 218/255, alpha: 1)
+        label.textAlignment = .center
+        label.layer.borderColor = CGColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
+        label.font = .systemFont(ofSize: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var backgroundGame: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: "bombGame")
+        element.contentMode = .scaleAspectFit
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var button: UIButton = {
+        var element = UIButton(type: .system)
+        element.titleLabel?.font = .systemFont(ofSize: 25)
+        element.backgroundColor = UIColor(red: 128/255, green: 49/255, blue: 167/255, alpha: 1)
+        element.layer.cornerRadius = 35
+        element.setTitle("Запустить", for: .normal)
+        element.setTitleColor(.yellow, for: .normal)
+        element.layer.borderWidth = 2
+
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setViews()
+        setupConstrains()
+    }
+    
+    //MARK: - Setup Views
+    
+    private func setViews() {
+        view.addSubview(backgroundView)
+        view.addSubview(mainTwoStackView)
+        mainTwoStackView.addArrangedSubview(mainOneStackView)
+        mainTwoStackView.addArrangedSubview(mainLabelView)
+        mainTwoStackView.addArrangedSubview(backgroundGame)
+        mainTwoStackView.addArrangedSubview(button)
+    }
+}
+
+    //MARK: - Setup Constraints
+
+extension GameViewController {
+    private func setupConstrains() {
+        NSLayoutConstraint.activate([
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            
+            mainTwoStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mainTwoStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mainTwoStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mainTwoStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            
+            mainLabelView.widthAnchor.constraint(equalToConstant: 314),
+            mainLabelView.heightAnchor.constraint(equalToConstant: 150),
+            
+            mainOneStackView.widthAnchor.constraint(equalToConstant: 314),
+            mainOneStackView.heightAnchor.constraint(equalToConstant: 75),
+            
+            backgroundGame.widthAnchor.constraint(equalToConstant: 312),
+            backgroundGame.heightAnchor.constraint(equalToConstant: 352),
+            
+            button.widthAnchor.constraint(equalToConstant: 274),
+            button.heightAnchor.constraint(equalToConstant: 79),
+        ])
+    }
+}
