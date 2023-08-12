2a2dbf4b255a94ae03a880c40c16d5f9268477b5
DmitryLorents <zmey.task@gmail.com>
2023-08-12 07:36:06 +0300

    Add UILabelExtension

--- Bomba-Challenge1/Extensions/UILabel+Extension.swift
+++ Bomba-Challenge1/Extensions/UILabel+Extension.swift
@@ -5,4 +5,13 @@
 //  Created by Дмитрий Лоренц on 12.08.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    convenience init(labelName: String) {
+        self.init()
+        self.font = .boldSystemFont(ofSize: 15)
+        self.text = labelName
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
