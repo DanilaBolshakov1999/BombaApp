244e1d76e56ca0c99c3c9a1c3a0578ddab949cc2
DmitryLorents <zmey.task@gmail.com>
2023-08-13 01:01:20 +0300

    Make gradient instead of background image view

--- Bomba-Challenge1/Controller/CategoryViewController.swift
+++ Bomba-Challenge1/Controller/CategoryViewController.swift
@@ -10,147 +10,164 @@ import UIKit
 
 // MARK: - CategoryViewController
 final class CategoryViewController: UIViewController {
-  
-  let gameData = GameData.shared
-
-  // MARK: - Private Property
-  private lazy var backgroundView: UIImageView = {
-    let backgroundView = UIImageView()
-    backgroundView.image = UIImage(named: "backgroundImage")
-    return backgroundView
-  }()
-  
-  private lazy var collectionView: UICollectionView = {
-    let layout = UICollectionViewFlowLayout()
-    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-    collectionView.backgroundColor = .clear
-    return collectionView
-  }()
-  
-  // MARK: - Override Methods
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    setupView()
-
-  }
-  
-  // MARK: - Actions Methods
-  @objc
-  func addTapped() {
-    navigationController?.popViewController(animated: true)
-  }
-  
+    
+    let gameData = GameData.shared
+    
+    // MARK: - Private Property
+    private let gradientLayer: CAGradientLayer = {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.yellow.cgColor, UIColor.orange.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.cornerRadius = 20
+        return gradientLayer
+    }()
+    private lazy var gradientBackgroundView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 20
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowRadius = 10
+        view.layer.shadowOffset = CGSize.zero
+        view.layer.shadowOpacity = 1
+        view.layer.addSublayer(gradientLayer)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
+    
+    // MARK: - Override Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gradientLayer.frame = gradientBackgroundView.bounds
+    }
+    
+    // MARK: - Actions Methods
+    @objc
+    func addTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
 }
 
 // MARK: - Setting Views
 private extension CategoryViewController {
-  func setupView() {
-    view.backgroundColor = .white
-    addSubviews()
-    setupLayout()
-    configureCollectionView()
-    configureNavController()
-  }
-  
+    func setupView() {
+        view.backgroundColor = .white
+        addSubviews()
+        setupLayout()
+        configureCollectionView()
+        configureNavController()
+    }
+    
 }
 
 // MARK: - Setting
 private extension CategoryViewController {
-  func addSubviews() {
-    view.addSubview(backgroundView)
-    view.addSubview(collectionView)
-  }
-  
-  func configureCollectionView() {
-    collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: CategoryCell.reuseIdentifier)
-    collectionView.delegate = self
-    collectionView.dataSource = self
-  }
-  
-  func configureNavController() {
-    title = "Категории"
-   
-    let appearance = UINavigationBarAppearance()
-    appearance.backgroundColor = .purple
-    appearance.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 25, weight: .bold),
-                                      NSAttributedString.Key.foregroundColor: UIColor(named: "purpleText") ?? .white]
-    navigationController?.navigationBar.standardAppearance = appearance
-    let leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.left"), style: .done, target: self, action: #selector(addTapped))
-    leftBarButtonItem.tintColor = .black
-    navigationItem.leftBarButtonItem = leftBarButtonItem
-  }
+    func addSubviews() {
+        view.addSubview(gradientBackgroundView)
+        view.addSubview(collectionView)
+    }
+    
+    func configureCollectionView() {
+        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: CategoryCell.reuseIdentifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    func configureNavController() {
+        title = "Категории"
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .purple
+        appearance.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 25, weight: .bold),
+                                          NSAttributedString.Key.foregroundColor: UIColor(named: "purpleText") ?? .white]
+        navigationController?.navigationBar.standardAppearance = appearance
+        let leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.left"), style: .done, target: self, action: #selector(addTapped))
+        leftBarButtonItem.tintColor = .black
+        navigationItem.leftBarButtonItem = leftBarButtonItem
+    }
 }
 
 // MARK: - Layout
 private extension CategoryViewController {
-  func setupLayout() {
-    backgroundView.translatesAutoresizingMaskIntoConstraints = false
-    collectionView.translatesAutoresizingMaskIntoConstraints = false
-    
-    NSLayoutConstraint.activate([
-      backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
-      backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-      
-      collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-      collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-    ])
-  }
+    func setupLayout() {
+        gradientBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            gradientBackgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            gradientBackgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            gradientBackgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            gradientBackgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
 }
 
 // MARK: - UICollectionViewDelegate, UICollectionViewDataSource
 extension CategoryViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-  
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return gameData.categories.count
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-  
-    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCell.reuseIdentifier, for: indexPath) as? CategoryCell else { return UICollectionViewCell() }
     
-    let category = gameData.categories[indexPath.row]
-    cell.imageView.image = UIImage(named: category.imageName)
-    cell.nameCategory.text = category.name
-
-    return cell
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    guard let cell = collectionView.cellForItem(at: indexPath) as? CategoryCell else { return }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return gameData.categories.count
+    }
     
-    gameData.categories[indexPath.row].isSelected = !gameData.categories[indexPath.row].isSelected
-    gameData.getQuestionArray()
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCell.reuseIdentifier, for: indexPath) as? CategoryCell else { return UICollectionViewCell() }
+        
+        let category = gameData.categories[indexPath.row]
+        cell.imageView.image = UIImage(named: category.imageName)
+        cell.nameCategory.text = category.name
+        
+        return cell
+    }
     
-    cell.selectCell = !cell.selectCell
-    cell.checkmark.image = cell.selectCell ? UIImage(named: "checkmarkOn") :  UIImage(named: "checkmarkOff")
-    print(gameData.choiceQusetions)
-  }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? CategoryCell else { return }
+        
+        gameData.categories[indexPath.row].isSelected = !gameData.categories[indexPath.row].isSelected
+        gameData.getQuestionArray()
+        
+        cell.selectCell = !cell.selectCell
+        cell.checkmark.image = cell.selectCell ? UIImage(named: "checkmarkOn") :  UIImage(named: "checkmarkOff")
+        print(gameData.choiceQusetions)
+    }
 }
 
 // MARK: - UICollectionViewDelegateFlowLayout
 extension CategoryViewController: UICollectionViewDelegateFlowLayout {
-  
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    let itemsPerRow: CGFloat = 2
-    let paddingWidth = 20 * (itemsPerRow + 1)
-    let availableWidth = collectionView.frame.width - paddingWidth
-    let widthPerItem = availableWidth / itemsPerRow
-    return CGSize(width: widthPerItem, height: widthPerItem)
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-    return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-    return 20
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-    return 20
-  }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemsPerRow: CGFloat = 2
+        let paddingWidth = 20 * (itemsPerRow + 1)
+        let availableWidth = collectionView.frame.width - paddingWidth
+        let widthPerItem = availableWidth / itemsPerRow
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
 }
